4daed18ca982c0166fada5c0b7d603c4a03cff3f
etgarP <etgar12340@gmail.com>
2024-11-26 12:28:58 +0200

    added the code that talks with the server

---
+++ server/services/talkToWsl.py
@@ -0,0 +1,39 @@
+import requests
+from PIL import Image
+import io
+
+# Define the URL of your Flask server
+url = 'http://localhost:5000/upload'
+
+# Paths of the images to be uploaded
+cloth_image_path = 'cloth.jpg'
+model_image_path = 'model.jpg'
+
+# Path where you want to save the cropped image
+cropped_image_path = 'triedOn.jpg'
+
+# Open the images in binary mode for uploading
+with open(cloth_image_path, 'rb') as cloth_file, open(model_image_path, 'rb') as model_file:
+    files = {
+        'cloth': ('cloth.jpg', cloth_file, 'image/jpeg'),
+        'model': ('model.jpg', model_file, 'image/jpeg')
+    }
+    
+    # Send a POST request with the images
+    response = requests.post(url, files=files)
+
+# Check the response from the server
+if response.status_code == 200:
+    # Open the received image from the response content
+    processed_image = Image.open(io.BytesIO(response.content))
+    
+    # Crop the right third of the image
+    width, height = processed_image.size
+    cropped_image = processed_image.crop((2 * width // 3, 0, width, height))
+    
+    # Save the cropped image
+    cropped_image.save(cropped_image_path)
+    print(f"Cropped image saved at: {cropped_image_path}")
+else:
+    print(f"Failed to upload images. Status code: {response.status_code}")
+    print("Error:", response.json())
